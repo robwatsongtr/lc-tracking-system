a3365267116aedba8cec11ceb4dea599fa742f6b
robwatsongtr <rwatso@gmail.com>
2025-06-29 14:11:56 -0700

    some comments

--- app/services/problem_service.py
+++ app/services/problem_service.py
@@ -1,6 +1,7 @@
 from app.db import database
 from app.models.Problem import Problem
 
+# 'Read'
 async def list_problems() -> list[Problem]:
     query = """
         SELECT id, lc_num, problem_name, problem_desc, problem_solution
@@ -9,3 +10,10 @@ async def list_problems() -> list[Problem]:
     rows = await database.fetch_all(query)
     # The ** is used to unpack a dictionary into keyword arguments.
     return [Problem(**row) for row in rows]
+
+# 'Create'
+
+# 'Update'
+
+# 'Delete' 
+
