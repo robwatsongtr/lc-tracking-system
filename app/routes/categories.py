a10094a3a28dee63beea78f9a6b9febaf6f26ed4
robwatsongtr <rwatso@gmail.com>
2025-07-16 14:42:15 -0700

    categories route:

--- app/routes/categories.py
+++ app/routes/categories.py
@@ -10,3 +10,24 @@ async def get_categories():
         return await category_service.list_categories()
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
+@router.post("/", response_model=Category)
+async def create_category(category: Category):
+    try:
+        return await category_service.create_category(category)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+@router.put("/{category_id}", response_model=Category)
+async def update_category(category_id: int, category: Category):
+    try:
+        return await category_service.update_category_by_id(category_id, category)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+@router.delete("/{category_id}")
+async def delete_category(category_id: int):
+    try:
+        return await category_service.delete_category_by_id(category_id)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
